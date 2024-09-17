87b2a21f1c83d01e1376b414a035017095c89f60
kush <kushbhardwaj8800@gmail.com>
2024-09-17 06:37:31 +0530

    messages form client to server, server to client

--- index.js
+++ index.js
@@ -13,8 +13,20 @@ app.get('/',function(req, res){
 //use socket
 io.on('connection',function(socket){
     console.log('A User Connected')
+
+    //send event to client || message server to client
+    setTimeout(function(){
+        socket.send("hello guys how are you")
+        socket.emit('customEvent',{data:"custom message from server side"})
+    },2000)
+    
     socket.on('disconnect',function(){
         console.log("A user Disconected")
+    })
+
+    //message from client
+    socket.on('myCustom',function(data){
+        console.log(data.data)
     })
 });
 http.listen("8080",()=>{
